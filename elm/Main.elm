b35caad3372ade44b1ece55e8920c5df4cd64a66
Adroit <cnnr_dhrty@live.com>
2019-04-01 19:08:08 -0400

    gahh

--- elm/Main.elm
+++ elm/Main.elm
@@ -349,7 +349,7 @@ viewUrl url =
             { defaultView | primaryView = TaskList (TaskList.Normal [ TaskList.IncompleteTasksOnly ] Nothing "It had a fragment!") }
 
         ( _, _ ) ->
-            parseIt url
+            { defaultView | primaryView = TaskList (TaskList.Normal [ TaskList.IncompleteTasksOnly ] Nothing "It had a fragment!") }
 
 
 routeParser : Parser (ViewState -> a) a
