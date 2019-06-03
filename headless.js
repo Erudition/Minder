42d88e6c9c92374f498f09ed247b8651db14dcc5
Adroit <cnnr_dhrty@live.com>
2019-06-03 01:53:13 -0400

    Yay! Hacked it together!

--- headless.js
+++ headless.js
@@ -11536,3 +11536,47 @@ _Platform_export({'Headless':{'init':author$project$Headless$main(
 							]))));
 		},
 		A2(elm$json$Json$Decode$index, 0, elm$json$Json$Decode$string)))(0)}});}(this));
+
+
+
+
+
+
+        //var storedState = localStorage.getItem('docket-v0.1-data');
+        //var startingState = storedState ? storedState : null;
+
+        var Elm = this.Elm; //trick I discovered to bypass importing
+
+        try {
+            var taskerIn = tk.global("ElmAppData");
+        } catch (e) {
+            var taskerIn = null;
+        }
+
+
+        var app = this.Elm.Headless.init({ flags: ["http://docket.app/?start=pet", taskerIn] });
+
+        app.ports.variableOut.subscribe(function(data) {
+            taskerOut(data[0], data[1]);
+        });
+
+        function taskerOut (name, value) {
+            try {
+                if (name.toLower == name)
+                  tk.setLocal(name, value);
+                else
+                  tk.setGlobal(name, value);
+            } catch (e) {
+                console.log("Setting " +name+ " to " +value+ " if tasker was here");
+            }
+        }
+
+
+        app.ports.setStorage.subscribe(function(state) {
+            taskerOut("ElmAppData", state);
+            console.log("storage set!");
+            console.log(state);
+            // done = true;
+        });
+
+        app.ports.headlessMsg.send("Message sent Is anybody home?");
--- headlessLaunch.js
+++ headlessLaunch.js
@@ -1,20 +1,39 @@
 
-//var Elm = require('./headless.js').Elm;
-import { Elm } from './headless.js';
+        //var storedState = localStorage.getItem('docket-v0.1-data');
+        //var startingState = storedState ? storedState : null;
 
-var storedState = localStorage.getItem('docket-v0.1-data');
-var startingState = storedState ? storedState : null;
+        var Elm = this.Elm; //trick I discovered to bypass importing
 
-var app = Elm.Headless.init({ flags: ["http://docket.app/?start=pet", startingState] });
+        try {
+            var taskerIn = tk.global("ElmAppData");
+        } catch (e) {
+            var taskerIn = null;
+        }
 
 
-app.ports.setStorage.subscribe(function(state) {
+        var app = this.Elm.Headless.init({ flags: ["http://docket.app/?start=pet", taskerIn] });
 
-    console.log("storage set!");
-    console.log(state);
-    // done = true;
-});
+        app.ports.variableOut.subscribe(function(data) {
+            taskerOut(data[0], data[1]);
+        });
 
-app.ports.headlessMsg.send("Message sent Is anybody home?");
+        function taskerOut (name, value) {
+            try {
+                if (name.toLower == name)
+                  tk.setLocal(name, value);
+                else
+                  tk.setGlobal(name, value);
+            } catch (e) {
+                console.log("Setting " +name+ " to " +value+ " if tasker was here");
+            }
+        }
 
-exit();
+
+        app.ports.setStorage.subscribe(function(state) {
+            taskerOut("ElmAppData", state);
+            console.log("storage set!");
+            console.log(state);
+            // done = true;
+        });
+
+        app.ports.headlessMsg.send("Message sent Is anybody home?");
