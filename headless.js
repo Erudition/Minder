9e7b2be2de1ba8f933e40d6183758ae55383d6d4
Adroit <cnnr_dhrty@live.com>
2019-06-03 12:12:32 -0400

    show wehn storage set

--- headless.js
+++ headless.js
@@ -11540,87 +11540,88 @@ _Platform_export({'Headless':{'init':author$project$Headless$main(
 
 //my helper functions:
 
-        function getVar (name) {
-            try {
-                return global(name);
-            } catch (e) {
-                return null;
-            }
-        }
+function getVar (name) {
+    try {
+        return global(name);
+    } catch (e) {
+        return null;
+    }
+}
 
-        function taskerOut (name, value) {
-            try {
-                if (name.toLower == name)
-                  setLocal(name, value);
-                else
-                  setGlobal(name, value);
-            } catch (e) {
-                logflash("Setting " +name+ " to " +value+ " if tasker was here");
-            }
-        }
+function taskerOut (name, value) {
+    try {
+        if (name.toLower == name)
+          setLocal(name, value);
+        else
+          setGlobal(name, value);
+    } catch (e) {
+        logflash("Setting " +name+ " to " +value+ " if tasker was here");
+    }
+}
 
-        function logflash(msg) {
-            try {
-                flash(msg);
-            } catch (e) {
-                console.log(msg);
-            }
-        }
+function logflash(msg) {
+    try {
+        flash(msg);
+    } catch (e) {
+        console.log(msg);
+    }
+}
 
-        function taskerTry (func) {
-            try {
-                return func();
-            } catch (e) {
-                console.log("Tried " + func);
-                return null;
-            }
-        }
+function taskerTry (func) {
+    try {
+        return func();
+    } catch (e) {
+        console.log("Tried " + func);
+        return null;
+    }
+}
 
-        function taskerReadAppData () {
-            try {
-                //return readFile("docket.dat");
-                return getVar("ElmAppData");
-            } catch (e) {
-                console.log("Tried to read file " + file);
-                return ' ';
-            }
-        }
+function taskerReadAppData () {
+    try {
+        //return readFile("docket.dat");
+        return getVar("ElmAppData");
+    } catch (e) {
+        console.log("Tried to read file " + file);
+        return ' ';
+    }
+}
 
 // Elm init
-        //var storedState = localStorage.getItem('docket-v0.1-data');
-        //var startingState = storedState ? storedState : null;
+//var storedState = localStorage.getItem('docket-v0.1-data');
+//var startingState = storedState ? storedState : null;
 
-        var Elm = this.Elm; //trick I discovered to bypass importing
-
-
-
-        var taskerUrl = getVar("elmUrl")
-        var taskerUrl = taskerUrl ? taskerUrl : "http://docket.app/?start=pet"
+var Elm = this.Elm; //trick I discovered to bypass importing
 
 
-        // touch file in case it's not There
-        //taskerTry(() => {writeFile("docket.dat","",true)});
 
-        var app = this.Elm.Headless.init(
-            { flags: [taskerUrl, taskerReadAppData()]
-            });
-
-         logflash("Running Elm! \n Url: "+ taskerUrl);
-
-        app.ports.variableOut.subscribe(function(data) {
-            taskerOut(data[0], data[1]);
-        });
-
-        app.ports.exit.subscribe(function(data) {
-          try {
-              exit()
-          } catch (e) {
-              console.log("Tried to exit, if tasker was here");
-          }
-        });
+var taskerUrl = getVar("elmUrl")
+var taskerUrl = taskerUrl ? taskerUrl : "http://docket.app/?start=pet"
 
 
-        app.ports.setStorage.subscribe(function(state) {
-            taskerOut("ElmAppData", state);
-            //taskerTry(() => {writeFile("docket.dat",state,false)});
-        });
+// touch file in case it's not There
+//taskerTry(() => {writeFile("docket.dat","",true)});
+
+var app = this.Elm.Headless.init(
+    { flags: [taskerUrl, taskerReadAppData()]
+    });
+
+ logflash("Running Elm! \n Url: "+ taskerUrl);
+
+app.ports.variableOut.subscribe(function(data) {
+    taskerOut(data[0], data[1]);
+});
+
+app.ports.exit.subscribe(function(data) {
+  try {
+      exit()
+  } catch (e) {
+      console.log("Tried to exit, if tasker was here");
+  }
+});
+
+
+app.ports.setStorage.subscribe(function(state) {
+    logflash("Storage set!");
+    taskerOut("ElmAppData", state);
+    //taskerTry(() => {writeFile("docket.dat",state,false)});
+});
