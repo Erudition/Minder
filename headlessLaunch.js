918841154898548ca061b18d6460fb3c8598d07f
Adroit <cnnr_dhrty@live.com>
2019-06-03 11:19:14 -0400

    touch file first, fix file not found

--- headlessLaunch.js
+++ headlessLaunch.js
@@ -1,39 +1,80 @@
 
+
+//my helper functions:
+
+        function getVar (name) {
+            try {
+                return global(name);
+            } catch (e) {
+                return null;
+            }
+        }
+
+        function taskerOut (name, value) {
+            try {
+                if (name.toLower == name)
+                  setLocal(name, value);
+                else
+                  setGlobal(name, value);
+            } catch (e) {
+                logflash("Setting " +name+ " to " +value+ " if tasker was here");
+            }
+        }
+
+        function logflash(msg) {
+            try {
+                flash(msg);
+            } catch (e) {
+                console.log(msg);
+            }
+        }
+
+        function taskerTry (func) {
+            try {
+                return func();
+            } catch (e) {
+                console.log("Tried " + func);
+                return null;
+            }
+        }
+
+// Elm init
         //var storedState = localStorage.getItem('docket-v0.1-data');
         //var startingState = storedState ? storedState : null;
 
         var Elm = this.Elm; //trick I discovered to bypass importing
 
-        try {
-            var taskerIn = tk.global("ElmAppData");
-        } catch (e) {
-            var taskerIn = null;
-        }
 
 
-        var app = this.Elm.Headless.init({ flags: ["http://docket.app/?start=pet", taskerIn] });
+        var taskerUrl = getVar("ElmUrl") ? getVar("ElmUrl") : "http://docket.app/?start=pet"
+
+
+        // touch file in case it's not There
+        taskerTry(() => {writeFile("docket.dat","",true)});
+
+        var app = this.Elm.Headless.init({ flags: [taskerUrl,
+                taskerTry(() => {readFile("docket.dat")}
+            )]
+         });
+
+         logflash("Running Elm! \n Url: "+ getVar("ElmUrl"));
 
         app.ports.variableOut.subscribe(function(data) {
             taskerOut(data[0], data[1]);
         });
 
-        function taskerOut (name, value) {
-            try {
-                if (name.toLower == name)
-                  tk.setLocal(name, value);
-                else
-                  tk.setGlobal(name, value);
-            } catch (e) {
-                console.log("Setting " +name+ " to " +value+ " if tasker was here");
-            }
-        }
+        app.ports.exit.subscribe(function(data) {
+          try {
+              exit()
+          } catch (e) {
+              console.log("Tried to exit, if tasker was here");
+          }
+        });
 
 
         app.ports.setStorage.subscribe(function(state) {
-            taskerOut("ElmAppData", state);
-            console.log("storage set!");
-            console.log(state);
-            // done = true;
+            //taskerOut("ElmAppData", state);
+            taskerTry(() => {writeFile("docket.dat",state,false)});
         });
 
         app.ports.headlessMsg.send("Message sent Is anybody home?");
