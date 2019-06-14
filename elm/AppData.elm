e897bc601f3a68f6ae5f1ea80bb8200c7b821105
Adroit <cnnr_dhrty@live.com>
2019-06-14 00:45:51 -0400

    Store Tasks in Dict instead of list

--- elm/AppData.elm
+++ elm/AppData.elm
@@ -1,10 +1,12 @@
 module AppData exposing (AppData, Instance, decodeAppData, encodeAppData, fromScratch, saveDecodeErrors, saveError, saveWarnings)
 
 import Activity.Activity as Activity exposing (..)
+import IntDict exposing (IntDict)
 import Json.Decode.Exploration as Decode exposing (..)
 import Json.Decode.Exploration.Pipeline as Pipeline exposing (..)
 import Json.Encode as Encode exposing (..)
 import List.Nonempty exposing (..)
+import Porting
 import Task.Progress exposing (..)
 import Task.Task exposing (..)
 import Task.TaskMoment exposing (..)
@@ -19,7 +21,7 @@ type alias Instance =
 type alias AppData =
     { uid : Instance
     , errors : List String
-    , tasks : List Task
+    , tasks : IntDict Task
     , activities : StoredActivities
     , timeline : Timeline
     , tokens : Tokens
@@ -30,7 +32,7 @@ fromScratch : AppData
 fromScratch =
     { uid = 0
     , errors = []
-    , tasks = []
+    , tasks = IntDict.empty
     , activities = []
     , timeline = []
     , tokens = emptyTokens
@@ -42,7 +44,7 @@ decodeAppData =
     Pipeline.decode AppData
         |> required "uid" Decode.int
         |> optional "errors" (Decode.list Decode.string) []
-        |> optional "tasks" (Decode.list decodeTask) []
+        |> optional "tasks" (Porting.decodeIntDict decodeTask) IntDict.empty
         |> optional "activities" Activity.decodeStoredActivities []
         |> optional "timeline" (Decode.list decodeSwitch) []
         |> optional "tokens" decodeTokens emptyTokens
@@ -51,7 +53,7 @@ decodeAppData =
 encodeAppData : AppData -> Encode.Value
 encodeAppData record =
     Encode.object
-        [ ( "tasks", Encode.list encodeTask record.tasks )
+        [ ( "tasks", Porting.encodeIntDict encodeTask record.tasks )
         , ( "activities", encodeStoredActivities record.activities )
         , ( "uid", Encode.int record.uid )
         , ( "errors", Encode.list Encode.string (List.take 100 record.errors) )
