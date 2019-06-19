0777633cd47ee9f5c5e444f12c0af31af6c1f2c4
Adroit <cnnr_dhrty@live.com>
2019-06-19 02:06:26 -0400

    Add new IntDict helper function

--- elm/IntDictExtra.elm
+++ elm/IntDictExtra.elm
@@ -1,4 +1,4 @@
-module IntDictExtra exposing (filterKeys, filterValues, mapValues)
+module IntDictExtra exposing (filterKeys, filterMap, filterValues, mapValues)
 
 import IntDict exposing (IntDict)
 
@@ -16,3 +16,31 @@ filterValues func dict =
 filterKeys : (Int -> Bool) -> IntDict v -> IntDict v
 filterKeys func dict =
     IntDict.filter (\k _ -> func k) dict
+
+
+{-| Apply a function that may or may not succeed to all entries in a dictionary,
+but only keep the successes.
+let
+isTeen n a =
+if 13 <= n && n <= 19 then
+Just <| String.toUpper a
+else
+Nothing
+in
+Dict.fromList [ ( 5, "Jack" ), ( 15, "Jill" ), ( 20, "Jones" ) ]
+|> filterMap isTeen
+--> Dict.fromList [ ( 15, "JILL" ) ]
+-}
+filterMap : (Int -> a -> Maybe b) -> IntDict a -> IntDict b
+filterMap f dict =
+    IntDict.foldl
+        (\k v acc ->
+            case f k v of
+                Just newVal ->
+                    IntDict.insert k newVal acc
+
+                Nothing ->
+                    acc
+        )
+        IntDict.empty
+        dict
