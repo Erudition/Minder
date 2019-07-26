22f3fa1970d9e7d4f30feb232597c2742c5b5c30
Adroit <cnnr_dhrty@live.com>
2019-07-25 20:46:28 -0400

    Flesh out remaining API commands

--- elm/External/Todoist/Command.elm
+++ elm/External/Todoist/Command.elm
@@ -7,6 +7,7 @@ Allows efficient batch processing and incremental sync.
 -}
 
 import Dict exposing (Dict)
+import External.Todoist.Item as Item exposing (..)
 import Http
 import ID
 import IntDict exposing (IntDict)
@@ -14,7 +15,7 @@ import IntDictExtra as IntDict
 import Json.Decode.Exploration as Decode exposing (..)
 import Json.Decode.Exploration.Pipeline exposing (..)
 import Json.Encode as Encode
-import Json.Encode.Extra as Encode2
+import Json.Encode.Extra as Encode
 import List.Extra as List
 import List.Nonempty exposing (Nonempty)
 import Maybe.Extra as Maybe
@@ -34,23 +35,35 @@ ProjectMove: First ID is the one you want to move. The second is the ID of the n
 -}
 type Command
     = ProjectAdd NewProject
-    | ProjectUpdate ProjectChanges
     | ProjectMove ProjectID (Maybe ProjectID)
     | ProjectDelete ProjectID
+    | ProjectUpdate ProjectChanges
     | ReorderProjects (List ProjectOrder)
+    | DayOrdersUpdate IDsToOrders
+    | ItemAdd NewItem
+    | ItemMoveProject (Maybe ProjectID)
+    | ItemMoveParent (Maybe ItemID)
+    | ItemDelete ItemID
+    | ItemClose ItemID
+    | ItemComplete ItemCompletion
+    | ItemCompleteRecurring RecurringItemCompletion
+    | ItemUncomplete ItemID
+    | ItemArchive ItemID
+    | ItemUnarchive ItemID
+    | ItemUpdate ItemChanges
+    | ReorderItems (List ItemOrder)
 
 
 encodeCommand : Command -> Encode.Value
 encodeCommand command =
     let
         encodeWrapper typeName args =
-            Encode.object <|
-                omitNothings
-                    [ normal ( "type", Encode.string typeName )
-                    , normal ( "args", args )
-                    , normal ( "uuid", Encode.string "UUID" ) -- TODO
-                    , omittable ( "temp_id", Encode.string, Nothing ) -- TODO
-                    ]
+            encodeObjectWithoutNothings
+                [ normal ( "type", Encode.string typeName )
+                , normal ( "args", args )
+                , normal ( "uuid", Encode.string "UUID" ) -- TODO
+                , omittable ( "temp_id", Encode.string, Nothing ) -- TODO
+                ]
     in
     case command of
         ProjectAdd new ->
@@ -61,16 +74,55 @@ encodeCommand command =
 
         ProjectMove id newParent ->
             encodeWrapper "project_move" <|
-                Encode.object
-                    [ ( "id", encodeProjectID id )
-                    , ( "parent_id", Encode2.maybe encodeProjectID newParent )
+                encodeObjectWithoutNothings
+                    [ normal ( "id", encodeProjectID id )
+                    , omittable ( "parent_id", encodeProjectID, newParent )
                     ]
 
         ProjectDelete id ->
             encodeWrapper "project_delete" <| Encode.object [ ( "id", encodeProjectID id ) ]
 
         ReorderProjects orderList ->
-            Encode.string "ReorderProjects"
+            encodeWrapper "project_reorder" <| Encode.list encodeProjectOrder orderList
+
+        DayOrdersUpdate _ ->
+            Debug.todo "handle DayOrdersUpdate _"
+
+        ItemAdd _ ->
+            Debug.todo "handle ItemAdd _"
+
+        ItemMoveProject _ ->
+            Debug.todo "handle ItemMoveProject _"
+
+        ItemMoveParent _ ->
+            Debug.todo "handle ItemMoveParent _"
+
+        ItemDelete _ ->
+            Debug.todo "handle ItemDelete _"
+
+        ItemClose _ ->
+            Debug.todo "handle ItemClose _"
+
+        ItemComplete _ ->
+            Debug.todo "handle ItemComplete _"
+
+        ItemCompleteRecurring _ ->
+            Debug.todo "handle ItemCompleteRecurring _"
+
+        ItemUncomplete _ ->
+            Debug.todo "handle ItemUncomplete _"
+
+        ItemArchive _ ->
+            Debug.todo "handle ItemArchive _"
+
+        ItemUnarchive _ ->
+            Debug.todo "handle ItemUnarchive _"
+
+        ItemUpdate _ ->
+            Debug.todo "handle ItemUpdate _"
+
+        ReorderItems _ ->
+            Debug.todo "handle ReorderItems _"
 
 
 type alias CommandUUID =
@@ -91,17 +143,17 @@ type alias BoolToInt =
 
 
 type ProjectID
-    = Real RealProjectID
-    | Temp TempID
+    = RealProject RealProjectID
+    | PlaceholderProject TempID
 
 
 encodeProjectID : ProjectID -> Encode.Value
 encodeProjectID realOrTemp =
     case realOrTemp of
-        Real intID ->
+        RealProject intID ->
             Encode.int intID
 
-        Temp tempID ->
+        PlaceholderProject tempID ->
             Encode.string tempID
 
 
@@ -116,24 +168,23 @@ type alias RealProjectID =
 type alias NewProject =
     { temp_id : Maybe TempID
     , name : String
-    , color : Int
+    , color : Maybe Int
     , parent_id : Maybe RealProjectID
-    , child_order : Int
+    , child_order : Maybe Int
     , is_favorite : BoolToInt
     }
 
 
 encodeNewProject : NewProject -> Encode.Value
 encodeNewProject new =
-    Encode.object <|
-        omitNothings
-            [ omittable ( "temp_id", Encode.string, new.temp_id )
-            , normal ( "name", Encode.string new.name )
-            , normal ( "color", Encode.int new.color )
-            , normal ( "parent_id", Encode2.maybe Encode.int new.parent_id )
-            , normal ( "child_order", Encode.int new.child_order )
-            , normal ( "is_favorite", Encode.bool new.is_favorite )
-            ]
+    encodeObjectWithoutNothings
+        [ omittable ( "temp_id", Encode.string, new.temp_id )
+        , normal ( "name", Encode.string new.name )
+        , omittable ( "color", Encode.int, new.color )
+        , omittable ( "parent_id", Encode.int, new.parent_id )
+        , omittable ( "child_order", Encode.int, new.child_order )
+        , normal ( "is_favorite", Encode.bool new.is_favorite )
+        ]
 
 
 {-| The fields required (and the only fields allowed) to ask the server to update an existing (or queued) Todoist "project", which can be done with the `ProjectUpdate` `Command`.
@@ -142,21 +193,20 @@ type alias ProjectChanges =
     { temp_id : Maybe TempID
     , name : String
     , color : Int
-    , collapsed : BoolToInt
-    , is_favorite : BoolToInt
+    , collapsed : BoolFromInt
+    , is_favorite : BoolFromInt
     }
 
 
 encodeProjectChanges : ProjectChanges -> Encode.Value
 encodeProjectChanges new =
-    Encode.object <|
-        omitNothings
-            [ omittable ( "temp_id", Encode.string, new.temp_id )
-            , normal ( "name", Encode.string new.name )
-            , normal ( "color", Encode.int new.color )
-            , normal ( "collapsed", Encode.bool new.collapsed )
-            , normal ( "is_favorite", Encode.bool new.is_favorite )
-            ]
+    encodeObjectWithoutNothings
+        [ omittable ( "temp_id", Encode.string, new.temp_id )
+        , normal ( "name", Encode.string new.name )
+        , normal ( "color", Encode.int new.color )
+        , normal ( "collapsed", Encode.bool new.collapsed )
+        , normal ( "is_favorite", Encode.bool new.is_favorite )
+        ]
 
 
 {-| One entry in a reordering list of projects.
@@ -172,6 +222,139 @@ encodeProjectOrder v =
     Encode.object
         [ ( "id", Encode.int v.id )
         , ( "child_order", Encode.int v.child_order )
+        ]
+
+
+
+--- ITEMS
+
+
+{-| The fields required (and the only fields allowed) to ask the server to create a new Todoist "item", which can be done with the `ItemAdd` `Command`.
+-}
+type alias NewItem =
+    { temp_id : Maybe TempID
+    , content : String
+    , project_id : Maybe ProjectID
+    , due : Maybe Due
+    , priority : Priority
+    , parent_id : Maybe ItemID
+    , child_order : Maybe Int
+    , day_order : Maybe Int
+    , collapsed : Maybe BoolFromInt
+    , auto_reminder : Maybe Bool
+    }
+
+
+encodeNewItem : NewItem -> Value
+encodeNewItem new =
+    encodeObjectWithoutNothings
+        [ omittable ( "temp_id", Encode.string, new.temp_id )
+        , normal ( "content", Encode.string new.content )
+        , omittable ( "project_id", encodeProjectID, new.project_id )
+        , omittable ( "due", encodeDue, new.due )
+        , normal ( "priority", encodePriority new.priority )
+        , omittable ( "parent_id", encodeItemID, new.parent_id )
+        , omittable ( "child_order", Encode.int, new.child_order )
+        , omittable ( "day_order", Encode.int, new.day_order )
+        , omittable ( "collapsed", encodeBoolToInt, new.collapsed )
+        , omittable ( "auto_reminder", Encode.bool, new.auto_reminder )
+        ]
+
+
+type ItemID
+    = RealItem RealItemID
+    | PlaceholderItem TempID
+
+
+encodeItemID : ItemID -> Encode.Value
+encodeItemID realOrTemp =
+    case realOrTemp of
+        RealItem intID ->
+            Encode.int intID
+
+        PlaceholderItem tempID ->
+            Encode.string tempID
+
+
+type alias ItemCompletion =
+    { id : ItemID
+    , date_completed : Maybe ISODateString
+    , force_history : Bool
+    }
+
+
+encodeItemCompletion : ItemCompletion -> Encode.Value
+encodeItemCompletion item =
+    encodeObjectWithoutNothings
+        [ normal ( "id", encodeItemID item.id )
+        , omittable ( "date_completed", Encode.string, item.date_completed )
+        , normal ( "force_history", Encode.bool item.force_history )
+        ]
+
+
+{-| API: Complete a recurring task, and the reason why this is a special case is because we need to mark a recurring completion (and using item\_update won’t do this). See also item\_close for a simplified version of the command.
+-}
+type alias RecurringItemCompletion =
+    { id : ItemID
+    , due : Maybe ISODateString
+    }
+
+
+encodeRecurringItemCompletion : RecurringItemCompletion -> Encode.Value
+encodeRecurringItemCompletion item =
+    encodeObjectWithoutNothings
+        [ normal ( "id", encodeItemID item.id )
+        , omittable ( "due", Encode.string, item.due )
+        ]
+
+
+{-| API: Updates task attributes. Please note that updating the parent, moving, completing or uncompleting tasks is not supported by item\_update, more specific commands have to be used instead.
+-}
+type alias ItemChanges =
+    { id : ItemID
+    , content : Maybe String
+    , due : Maybe Due
+    , priority : Maybe Priority
+    , day_order : Maybe Int
+    , collapsed : Maybe BoolFromInt
+    }
+
+
+encodeItemChanges : ItemChanges -> Value
+encodeItemChanges item =
+    encodeObjectWithoutNothings
+        [ normal ( "id", encodeItemID item.id )
+        , omittable ( "content", Encode.string, item.content )
+        , omittable ( "due", encodeDue, item.due )
+        , omittable ( "priority", encodePriority, item.priority )
+        , omittable ( "day_order", Encode.int, item.day_order )
+        , omittable ( "collapsed", encodeBoolToInt, item.collapsed )
+        ]
+
+
+type alias DayOrder =
+    Int
+
+
+{-| API says: A dictionary, where an item id is the key, and the day order its value: item\_id: day\_order.
+-}
+type alias IDsToOrders =
+    IntDict DayOrder
+
+
+{-| One entry in a reordering list of items.
+-}
+type alias ItemOrder =
+    { id : RealItemID
+    , child_order : Int
+    }
+
+
+encodeItemOrder : ItemOrder -> Encode.Value
+encodeItemOrder order =
+    Encode.object
+        [ ( "id", Encode.int order.id )
+        , ( "child_order", Encode.int order.child_order )
         ]
 
 
