46c7e766dd8b4942148cceb005d30d0c0371bf23
Adroit <cnnr_dhrty@live.com>
2019-08-05 17:29:54 -0400

    oops

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ elm make --output=www/elm-headless.js --optimize elm/Headless.elm
 
 (echo "var XMLHttpRequest = require('node-http-xhr');" && cat www/elm-headless.js) > www/elm-headless.js.tmp && mv www/elm-headless.js.tmp www/elm-headless.js
 
+# run
 node --interactive www/headless-launch.js $1
 
 sed -i '1d' www/elm-headless.js
