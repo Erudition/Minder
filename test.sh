77a8ba438b191f6fb63747752218f5646ec82a8c
Adroit <cnnr_dhrty@live.com>
2019-08-05 17:23:24 -0400

    get rid of require after node runs

--- test.sh
+++ test.sh
@@ -6,3 +6,5 @@ elm make --output=www/elm-headless.js elm/Headless.elm
 (echo "var XMLHttpRequest = require('node-http-xhr');" && cat www/elm-headless.js) > www/elm-headless.js.tmp && mv www/elm-headless.js.tmp www/elm-headless.js
 
 node --interactive www/headless-launch.js $1
+
+sed -i '1d' www/elm-headless.js
