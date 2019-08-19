6adbd56afa1893b5d2ca391947bba28d5a534204
Adroit <cnnr_dhrty@live.com>
2019-08-19 02:40:27 -0400

    improved testing script to work with missing files and directories

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 cd ~/Projects/Elm/Docket/
 
-elm make --output=www/elm-gui.js elm/Main.elm
-elm make --output=www/elm-headless.js elm/Headless.elm
+elm make --output=www/elm-gui.js --optimize elm/Main.elm
+elm make --output=www/elm-headless.js --optimize elm/Headless.elm
 
+# Add line to elm that makes it's HTTP transparently compatible with node
 (echo "var XMLHttpRequest = require('node-http-xhr');" && cat www/elm-headless.js) > www/elm-headless.js.tmp && mv www/elm-headless.js.tmp www/elm-headless.js
 
-# run
+# make a temp Minder dir if it doesn't exist
+mkdir -p /tmp/Minder
+
+# put some data in the personal file
+# echo "{"tasks":[],"activities":[],"uid":0,"errors":[],"timeline":[{"Time":63701879628159,"Activity":54}],"todoist":{"cache":{"nextSync":"*","items":[],"projects":[],"pendingCommands":[]},"activityProjectIDs":[]}}" > /tmp/Minder/personal-data.json
+
+# run node
 node --interactive www/headless-launch.js $1
 
+# remove that line from the compiled elm so it works with non-node
 sed -i '1d' www/elm-headless.js
