352fa1c74d9616082acb2658cc6cadc24188aa9f
Adroit <cnnr_dhrty@live.com>
2019-08-19 01:05:31 -0400

    move to more general alarm type

--- elm/Activity/Reminder.elm
+++ elm/Activity/Reminder.elm
@@ -1,6 +1,10 @@
-module Activity.Reminder exposing (Alarm, Intent, NotificationAction, Reminder, scheduleExcusedReminders, scheduleOffTaskReminders, scheduleOnTaskReminders)
+module Activity.Reminder exposing (Action(..), Alarm, Intent, NotificationAction, encodeAction, encodeAlarm, reminder, reminder3, scheduleExcusedReminders, scheduleOffTaskReminders, scheduleOnTaskReminders)
 
+import External.Notification as Notif exposing (Notification)
+import Json.Encode as Encode
+import Json.Encode.Extra as Encode
 import List.Extra as List
+import Porting exposing (encodeMoment)
 import SmartTime.Duration as Duration exposing (..)
 import SmartTime.Human.Duration as HumanDuration exposing (HumanDuration(..), abbreviatedSpaced, breakdownHM, dur)
 import SmartTime.Moment as Moment exposing (Moment, future, past)
@@ -10,11 +14,19 @@ type alias Intent =
     String
 
 
-type alias Reminder =
-    { scheduledFor : Moment
-    , title : String
-    , subtitle : String
-    , actions : List NotificationAction
+reminder : { scheduledFor : Moment, title : String, subtitle : String } -> Alarm
+reminder { scheduledFor, title, subtitle } =
+    { schedule = scheduledFor
+    , actions =
+        [ Notify <| Notif.basic title subtitle ]
+    }
+
+
+reminder3 : Moment -> String -> String -> Alarm
+reminder3 scheduledFor title subtitle =
+    { schedule = scheduledFor
+    , actions =
+        [ Notify <| Notif.basic title subtitle ]
     }
 
 
@@ -31,126 +43,65 @@ type alias Alarm =
     }
 
 
+encodeAlarm : Alarm -> Encode.Value
+encodeAlarm v =
+    Encode.object
+        [ ( "schedule", encodeMoment v.schedule )
+        , ( "actions", Encode.list encodeAction v.actions )
+        ]
+
+
 type Action
     = Notify Notification
-    | RunTaskerTask
+    | RunTaskerTask String String
     | SendIntent
 
 
-type alias Notification =
-    { id : String -- 1
-    , persistent : Bool -- 2
-    , timeout : Duration -- 3
-    , update : UpdateStrategy -- 4: not supported in autonotification variables
-    , priority : Priority -- 5
-    , privacy : Privacy -- 6: not supported in autonotification variables
-    , useHTML : Bool -- 7: not supported in autonotification variables
-    , title : String -- 8
-    , titleExpanded : String -- 9
-    , body : String -- 10 ("text")
-    , bodyExpanded : String -- 11 ("text expanded")
-    , subtext : String -- 12
-    , detail : Detail -- 13 & 14: need to be Int?
-    , ticker : String -- 15
-    , icon : String -- 16
-    , statusIcon : String -- 17
-    , statusTextSize : Int -- 18 (in sp)
-    , backgroundColor : String -- 19
-    , colorFromMedia : Bool -- 20: not supported in autonotification variables
-    , badge : BadgeType -- 21:  not supported in autonotification variables
-    , picture : String -- 22
-    , pictureCacheAllowed : Bool -- 23  not supported in autonotification variables
-    , pictureExpandedIcon : String -- 24
-    , mediaLayout : Bool -- 25 not supported in autonotification variables
-    , media : Maybe MediaInfo -- 26
-    , url : String --29
-    , time : Maybe Moment
-    , chronometer : Bool
-    , countdown : Bool
-    }
+encodeAction : Action -> Encode.Value
+encodeAction v =
+    case v of
+        Notify notif ->
+            Encode.object [ ( "notify", Notif.encodeNotification notif ) ]
+
+        RunTaskerTask name param ->
+            Encode.string "RunTaskerTask"
+
+        SendIntent ->
+            Encode.string "SendIntent"
 
 
-type Priority
-    = Default
-    | Low
-    | High
-    | Min
-    | Max
-
-
-type Privacy
-    = Public
-    | Private
-    | PrivateWithPublicVersion Notification
-    | Secret
-
-
-type UpdateStrategy
-    = New
-    | Replace
-    | Append
-
-
-type Detail
-    = Number Int
-    | Info String
-
-
-type BadgeType
-    = NoBadge
-    | SmallIcon
-    | LargeIcon
-
-
-type alias MediaInfo =
-    { title : String
-    }
-
-
-type alias Events msg =
-    { onCreate : Maybe msg --27
-    , onTouch : Maybe msg --28
-    , onDismiss : Maybe msg --30
-    }
-
-
-scheduleOnTaskReminders : Moment -> Duration -> List Reminder
+scheduleOnTaskReminders : Moment -> Duration -> List Alarm
 scheduleOnTaskReminders now fromNow =
     let
         fractionLeft denom =
             future now <| Duration.subtract fromNow (Duration.scale fromNow (1 / denom))
     in
-    [ Reminder (fractionLeft 2)
+    [ reminder3 (fractionLeft 2)
         "Half-way done!"
         "1/2 time left for activity."
-        []
-    , Reminder (fractionLeft 3)
+    , reminder3 (fractionLeft 3)
         "Two-thirds done!"
         "1/3 time left for activity."
-        []
-    , Reminder (fractionLeft 4)
+    , reminder3 (fractionLeft 4)
         "Three-Quarters done!"
         "1/4 time left for activity."
-        []
-    , Reminder (future now fromNow)
+    , reminder3 (future now fromNow)
         "Time's up!"
         "Reached maximum time allowed for this."
-        []
     ]
 
 
-scheduleOffTaskReminders : Moment -> List Reminder
+scheduleOffTaskReminders : Moment -> List Alarm
 scheduleOffTaskReminders now =
-    [ Reminder now
+    [ reminder3 now
         "Get back on task now!"
         "Off task, not excused!"
-        []
     ]
 
 
 {-| Calculate the interim reminders before the activity expires from being excused.
 -}
-scheduleExcusedReminders : Moment -> Duration -> Duration -> List Reminder
+scheduleExcusedReminders : Moment -> Duration -> Duration -> List Alarm
 scheduleExcusedReminders now excusedLimit timeLeft =
     let
         firstIsGreater first last =
@@ -186,28 +137,31 @@ scheduleExcusedReminders now excusedLimit timeLeft =
                 ]
 
         buildGettingCloseReminder amountLeft =
-            { scheduledFor = beforeTimesUp amountLeft
-            , title = "Finish up! Only " ++ write amountLeft ++ " left!"
-            , subtitle = "Excused for up to " ++ write excusedLimit
-            , actions = []
-            }
+            reminder
+                { scheduledFor = beforeTimesUp amountLeft
+                , title = "Finish up! Only " ++ write amountLeft ++ " left!"
+                , subtitle = "Excused for up to " ++ write excusedLimit
+                }
 
         write durLeft =
             abbreviatedSpaced <| breakdownHM durLeft
 
         interimReminders =
-            [ { scheduledFor = future now (dur (Minutes 10))
-              , title = "Distraction taken care of?"
-              , subtitle = "Get back on task as soon as possible - do this later!"
-              }
-            , { scheduledFor = future now (dur (Minutes 20))
-              , title = "Ready to get back on task?"
-              , subtitle = "You have important goals to meet!"
-              }
-            , { scheduledFor = future now (dur (Minutes 30))
-              , title = "Can this wait?"
-              , subtitle = "Why not put this in your task list for later?"
-              }
+            [ reminder
+                { scheduledFor = future now (dur (Minutes 10))
+                , title = "Distraction taken care of?"
+                , subtitle = "Get back on task as soon as possible - do this later!"
+                }
+            , reminder
+                { scheduledFor = future now (dur (Minutes 20))
+                , title = "Ready to get back on task?"
+                , subtitle = "You have important goals to meet!"
+                }
+            , reminder
+                { scheduledFor = future now (dur (Minutes 30))
+                , title = "Can this wait?"
+                , subtitle = "Why not put this in your task list for later?"
+                }
             ]
     in
     if substantialTimeLeft then
