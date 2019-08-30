15163b15caaac219fca9f26b51512ad8b389516e
Adroit <cnnr_dhrty@live.com>
2019-08-29 22:15:38 -0400

    Totally new reminder set, for new notifications

--- elm/Activity/Reminder.elm
+++ elm/Activity/Reminder.elm
@@ -1,4 +1,4 @@
-module Activity.Reminder exposing (Action(..), Alarm, Intent, NotificationAction, scheduleExcusedReminders, scheduleOffTaskReminders, scheduleOnTaskReminders)
+module Activity.Reminder exposing (scheduleExcusedReminders, scheduleOffTaskReminders, scheduleOnTaskReminders)
 
 import Json.Encode as Encode
 import Json.Encode.Extra as Encode
@@ -11,145 +11,87 @@ import SmartTime.Human.Duration as HumanDuration exposing (HumanDuration(..), ab
 import SmartTime.Moment as Moment exposing (Moment, future, past)
 
 
-type alias Intent =
-    String
-
-
-reminder : { scheduledFor : Moment, title : String, subtitle : String } -> Alarm
-reminder { scheduledFor, title, subtitle } =
-    { schedule = scheduledFor
-    , actions =
-        [ Notify <| Notif.basic "reminder" (Duration.fromMinutes 5) title subtitle ]
-    }
-
-
-reminderZap : { scheduledFor : Moment, title : String, subtitle : String } -> Alarm
-reminderZap { scheduledFor, title, subtitle } =
-    { schedule = scheduledFor
-    , actions =
-        [ Notify <| Notif.basic "reminder" (Duration.fromMinutes 5) title subtitle
-        , RunTaskerTask "Pavlok Zap" ""
-        ]
-    }
-
-
-type alias NotificationAction =
-    { label : String
-    , action : Intent
-    , icon : String
-    }
-
-
-type alias Alarm =
-    { schedule : Moment
-    , actions : List Action
-    }
-
-
-encodeAlarm : Alarm -> Encode.Value
-encodeAlarm v =
-    Encode.object
-        [ ( "schedule", Encode.int <| Moment.toUnixTimeInt v.schedule )
-        , ( "actions", Encode.list encodeAction v.actions )
-        ]
-
-
-type Action
-    = Notify Notification
-    | RunTaskerTask String String
-    | SendIntent
-
-
-scheduleNotify : List Alarm -> Cmd msg
-scheduleNotify alarmList =
+reminderBase : Notification
+reminderBase =
     let
-        getNotification alarm =
-            List.map forEachAction alarm.actions
-
-        forEachAction action =
-            case action of
-                Notify notif ->
-                    scheduleNotification (Notif.encodeNotification notif)
-
-                RunTaskerTask name params ->
-                    Cmd.none
-
-                SendIntent ->
-                    Cmd.none
+        blank =
+            Notif.blank
     in
-    Cmd.batch <| List.concat (List.map getNotification alarmList)
+    { blank | timeout = Just (Duration.fromMinutes 1) }
 
 
-encodeAction : Action -> Encode.Value
-encodeAction v =
-    case v of
-        Notify notif ->
-            Encode.object [ ( "notify", Notif.encodeNotification notif ) ]
-
-        RunTaskerTask name parameters ->
-            Encode.object [ ( "task_name", Encode.string name ), ( "task_par", Encode.string parameters ) ]
-
-        SendIntent ->
-            Encode.string "SendIntent"
-
-
-scheduleOnTaskReminders : Moment -> Duration -> List Alarm
-scheduleOnTaskReminders now fromNow =
+scheduleOnTaskReminders : Moment -> Duration -> List Notification
+scheduleOnTaskReminders now timeLeft =
     let
         fractionLeft denom =
-            future now <| Duration.subtract fromNow (Duration.scale fromNow (1 / denom))
+            future now <| Duration.subtract timeLeft (Duration.scale timeLeft (1 / denom))
     in
-    [ reminder
-        { scheduledFor = fractionLeft 2
-        , title = "Half-way done!"
-        , subtitle = "1/2 time left for activity."
-        }
-    , reminder
-        { scheduledFor = fractionLeft 3
-        , title = "Two-thirds done!"
-        , subtitle = "1/3 time left for activity."
-        }
-    , reminder
-        { scheduledFor = fractionLeft 4
-        , title = "Three-Quarters done!"
-        , subtitle = "1/4 time left for activity."
-        }
-    , reminder
-        { scheduledFor = future now fromNow
-        , title = "Time's up!"
-        , subtitle = "Reached maximum time allowed for this."
-        }
+    [ { reminderBase
+        | at = Just <| fractionLeft 2
+        , title = Just "Half-way done!"
+        , subtitle = Just "1/2 time left for activity."
+      }
+    , { reminderBase
+        | at = Just <| fractionLeft 3
+        , title = Just "Two-thirds done!"
+        , subtitle = Just "1/3 time left for activity."
+      }
+    , { reminderBase
+        | at = Just <| fractionLeft 4
+        , title = Just "Three-quarters done!"
+        , subtitle = Just "1/4 time left for activity."
+      }
+    , { reminderBase
+        | at = Just <| future now timeLeft
+        , title = Just "Three-quarters done!"
+        , subtitle = Just "1/4 time left for activity."
+      }
     ]
 
 
-scheduleOffTaskReminders : Moment -> List Alarm
+scheduleOffTaskReminders : Moment -> List Notification
 scheduleOffTaskReminders now =
-    [ reminder
-        { scheduledFor = now
-        , title = "Off Task!"
-        , subtitle = "You can do this later"
-        }
-    , reminderZap
-        { scheduledFor = future now (Duration.fromSeconds 30.0)
-        , title = "Off Task! Warning 2"
-        , subtitle = "You have more important things to do right now!"
-        }
-    , reminderZap
-        { scheduledFor = future now (Duration.fromSeconds 60.0)
-        , title = "Off Task! Warning 3"
-        , subtitle = "You have more important things to do right now!"
-        }
-    , reminderZap
-        { scheduledFor = future now (Duration.fromSeconds 90.0)
-        , title = "Off Task! Warning 4"
-        , subtitle = "You have more important things to do right now!"
-        }
+    let
+        base =
+            { reminderBase
+                | id = Just 1
+                , channel = Just "Off Task Warnings"
+                , actions = actions
+            }
+
+        actions =
+            [ { id = "SnoozeButton", button = Notif.Button "Snooze", launch = False }
+            , { id = "LaunchButton", button = Notif.Button "Go", launch = True }
+            , { id = "ZapButton", button = Notif.Button "Zap", launch = False }
+            ]
+    in
+    [ { base
+        | at = Just <| now
+        , subtitle = Just "Off Task!"
+        , title = Just "You can do this later"
+      }
+    , { base
+        | at = Just <| future now (Duration.fromSeconds 30.0)
+        , subtitle = Just "Off Task! Second Warning"
+        , title = Just "You have more important things to do right now!"
+      }
+    , { base
+        | at = Just <| future now (Duration.fromSeconds 60.0)
+        , subtitle = Just "Off Task! Third Warning"
+        , title = Just "You have more important things to do right now!"
+      }
+    , { base
+        | at = Just <| future now (Duration.fromSeconds 90.0)
+        , subtitle = Just "Off Task!"
+        , title = Just "You have more important things to do right now!"
+        , interval = Just Notif.Minute
+      }
     ]
 
 
 {-| Calculate the interim reminders before the activity expires from being excused.
 -}
-scheduleExcusedReminders : Moment -> Duration -> Duration -> List Alarm
+scheduleExcusedReminders : Moment -> Duration -> Duration -> List Notification
 scheduleExcusedReminders now excusedLimit timeLeft =
     let
         firstIsGreater first last =
@@ -185,31 +127,31 @@ scheduleExcusedReminders now excusedLimit timeLeft =
                 ]
 
         buildGettingCloseReminder amountLeft =
-            reminder
-                { scheduledFor = beforeTimesUp amountLeft
-                , title = "Finish up! Only " ++ write amountLeft ++ " left!"
-                , subtitle = "Excused for up to " ++ write excusedLimit
-                }
+            { reminderBase
+                | at = Just <| beforeTimesUp amountLeft
+                , title = Just <| "Finish up! Only " ++ write amountLeft ++ " left!"
+                , subtitle = Just <| "Excused for up to " ++ write excusedLimit
+            }
 
         write durLeft =
             abbreviatedSpaced <| HumanDuration.breakdownNonzero durLeft
 
         interimReminders =
-            [ reminder
-                { scheduledFor = future now (dur (Minutes 10))
-                , title = "Distraction taken care of?"
-                , subtitle = pickEncouragementMessage (future now (dur (Minutes 10)))
-                }
-            , reminder
-                { scheduledFor = future now (dur (Minutes 20))
-                , title = "Ready to get back on task?"
-                , subtitle = pickEncouragementMessage (future now (dur (Minutes 20)))
-                }
-            , reminder
-                { scheduledFor = future now (dur (Minutes 30))
-                , title = "Can this wait?"
-                , subtitle = pickEncouragementMessage (future now (dur (Minutes 30)))
-                }
+            [ { reminderBase
+                | at = Just <| future now (dur (Minutes 10))
+                , title = Just "Distraction taken care of?"
+                , subtitle = Just <| pickEncouragementMessage (future now (dur (Minutes 10)))
+              }
+            , { reminderBase
+                | at = Just <| future now (dur (Minutes 20))
+                , title = Just "Ready to get back on task?"
+                , subtitle = Just <| pickEncouragementMessage (future now (dur (Minutes 20)))
+              }
+            , { reminderBase
+                | at = Just <| future now (dur (Minutes 30))
+                , title = Just "Can this wait?"
+                , subtitle = Just <| pickEncouragementMessage (future now (dur (Minutes 30)))
+              }
             ]
 
         pickEncouragementMessage time =
