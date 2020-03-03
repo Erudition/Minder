7cdcbda71f03474548d01f478d605eb8a6755767
Erudition <cnnr_dhrty@live.com>
2020-03-03 17:00:10 -0500

    add new strict-length Int parser

--- elm/ParserExtra.elm
+++ elm/ParserExtra.elm
@@ -1,21 +1,23 @@
-module ParserExtra exposing (deadEndsToString, paddedInt, realDeadEndsToString, strictPaddedInt)
+module ParserExtra exposing (deadEndsToString, possiblyPaddedInt, realDeadEndsToString, strictLengthInt, strictPaddedInt)
 
 import Parser exposing (..)
 
 
-paddedInt : Parser Int
-paddedInt =
+possiblyPaddedInt : Parser Int
+possiblyPaddedInt =
     getChompedString (chompWhile Char.isDigit)
         |> Parser.andThen digitStringToInt
 
 
 {-| Like `paddedInt`, but fails if the integer was not padded enough to meet a minimum length.
 
-Perhaps you want to parse the time of day:
+Perhaps you want to parse a 4+ digit year, avoiding the potential ambiguity of short years:
 
-    "1:05" -- allowed
+    "1998" -- allowed
 
-    "1:5" -- not allowed
+    "98" -- not allowed (could mean '98)
+
+    "0098" -- allowed (a real year!)
 
 Note: there is no maximum length check, but you can just check the successfully parsed `Int` if it's greater than you prefer. For example, you can use this to parse the year `2004` and `0098` while failing on `98`, but the year `12004` will always succeed (it's a valid year!) because you could always do `yearInt <= 9999` if you want to exclude 5-digit years.
 
@@ -27,6 +29,39 @@ strictPaddedInt minLength =
         checkSize digits =
             if String.length digits >= minLength then
                 Parser.succeed digits
+
+            else
+                Parser.problem <| "Found number: " ++ digits ++ " but it was not padded to a minimum of " ++ String.fromInt minLength ++ " digits long."
+    in
+    getChompedString (chompWhile Char.isDigit)
+        |> Parser.andThen checkSize
+        |> Parser.andThen digitStringToInt
+
+
+{-| Like `strictPaddedInt`, but with a maximum digit count.
+
+Perhaps you want to parse the time of day:
+
+    "1:05" -- allowed
+
+    "1:5" -- not allowed
+
+    "1:537" -- not allowed
+
+Note: there is a maximum length check, but use it wisely - you can always just check the successfully parsed `Int` if it's greater than you prefer. For example, you can use this to parse the year `2004` and `0098` while failing on `98`, but let the year `12004` succeed (it's a valid year!) unless you want to exclude >4-digit years.
+
+-}
+strictLengthInt : Int -> Int -> Parser Int
+strictLengthInt minLength maxLength =
+    let
+        checkSize : String -> Parser String
+        checkSize digits =
+            if String.length digits >= minLength then
+                if String.length digits <= maxLength then
+                    Parser.succeed digits
+
+                else
+                    Parser.problem <| "Found number: " ++ digits ++ " but it exceeded the maximum of " ++ String.fromInt maxLength ++ " digits long."
 
             else
                 Parser.problem <| "Found number: " ++ digits ++ " but it was not padded to a minimum of " ++ String.fromInt minLength ++ " digits long."
