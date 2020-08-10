47f4c39a5625469ad8849be32653884a930b7aac
Erudition <cnnr_dhrty@live.com>
2020-08-10 14:47:49 -0500

    start using Elm-live, fix build script

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ cd ~/Projects/Minder/
 
 elm make --output=www/elm-gui.js  elm/Main.elm
 elm make --output=www/elm-headless.js  elm/Headless.elm
+
+
+elm-live elm/Main.elm --start-page=webapp.html --dir=www --pushstate  --  --output=www/elm-gui.js
