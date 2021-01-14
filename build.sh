143a183014ccf5406b4e9aa5a4c8b1870174cacf
Erudition <cnnr_dhrty@live.com>
2021-01-13 18:59:11 -0600

    Attempt to get Nativescript working again

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ elm make --output=www/elm-gui.js  elm/Main.elm
 elm make --output=www/elm-headless.js  elm/Headless.elm
 
 
-elm-live elm/Main.elm --start-page=webapp.html --dir=www --pushstate  --  --output=www/elm-gui.js
+elm-live elm/Main.elm --dir=www --pushstate --ssl --  --output=www/elm-gui.js
