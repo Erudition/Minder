142d55b22a6f6b50b3eeb3ab06529ff78e4d3597
Erudition <cnnr_dhrty@live.com>
2021-09-22 21:31:04 -0500

    start logic to parse ops from strings

--- elm/Replicated/Op/Op.elm
+++ elm/Replicated/Op/Op.elm
@@ -5,6 +5,7 @@ import List.Extra
 import List.Nonempty exposing (Nonempty)
 import Replicated.Op.OpID as OpID exposing (OpID)
 import Replicated.Serialize as RS exposing (Codec)
+import Result.Extra
 import Set exposing (Set)
 import SmartTime.Moment as Moment
 
@@ -155,18 +156,128 @@ fromString inputString previousOpMaybe =
 
         remainderPayload =
             String.concat otherAtoms
+
+        reducerMaybe =
+            case ( referenceAtom, previousOpMaybe ) of
+                -- TODO check an actual list of known reducers
+                ( ":lww", _ ) ->
+                    Just "lww"
+
+                ( _, Just previousOp ) ->
+                    Just (reducer previousOp)
+
+                ( _, Nothing ) ->
+                    Nothing
+
+        resultWithReducer givenReducer =
+            case ( opIDMaybe, otherAtoms, referenceMaybe ) of
+                ( Just opID, [], _ ) ->
+                    -- no payload - must be a creation op
+                    Ok (create givenReducer opID opID Nothing "")
+
+                ( Just opID, _, Just ref ) ->
+                    -- there's a payload - reference is required
+                    case Maybe.map object previousOpMaybe of
+                        Just objectLastTime ->
+                            Ok (create givenReducer objectLastTime opID (Just ref) remainderPayload)
+
+                        Nothing ->
+                            -- TODO locate object via tree somehow
+                            Err <| "Couldn't determine the object this op applies to: " ++ inputString
+
+                ( Just _, _, Nothing ) ->
+                    Err <| "This op has a nonempty payload (not a creation op) but I couldn't find the required *reference* atom (:) and got no prior op in the chain to deduce it from: " ++ inputString
+
+                ( Nothing, _, _ ) ->
+                    Err "Couldn't find Op's ID (@) and got no prior op to deduce it from."
     in
-    case ( opIDMaybe, otherAtoms, referenceMaybe ) of
-        ( Just opID, [], _ ) ->
-            -- no payload - must be a creation op
-            Ok (create givenReducer opID opID Nothing "")
+    case reducerMaybe of
+        Just foundReducer ->
+            resultWithReducer foundReducer
 
-        ( Just opID, _, Just ref ) ->
-            -- there's a payload - reference is required
-            Ok (create givenReducer givenObject opID (Just ref) remainderPayload)
+        Nothing ->
+            Err <| "No reducer found for op: " ++ inputString
 
-        ( Just _, _, Nothing ) ->
-            Err <| "This op has a nonempty payload (not a creation op) but I couldn't find the required *reference* atom (:) and got no prior op in the chain to deduce it from: " ++ inputString
 
-        ( Nothing, _, _ ) ->
-            Err "Couldn't find Op's ID (@) and got no prior op to deduce it from."
+{-| A span is [or is part of] a chain (sequential refs) with sequential IDs as well.
+
+Here we pass in the previously parsed Op (when available) to parsing of the next op, so we may derive its ID implicitly
+
+-}
+fromSpan : String -> Result String (List Op)
+fromSpan span =
+    let
+        spanOpStrings =
+            String.split "," span
+
+        addToOpList unparsedOpList parsedOpListResult =
+            -- Definitely a more efficient FP way to do this
+            case ( unparsedOpList, parsedOpListResult ) of
+                ( _, Err err ) ->
+                    -- we crashed somewhere.
+                    Err err
+
+                ( [], _ ) ->
+                    -- nothing to parse.
+                    Ok []
+
+                ( [ unparsedOp ], Ok [] ) ->
+                    -- not started yet, one item to parse
+                    Result.map List.singleton (fromString unparsedOp Nothing)
+
+                ( nextUnparsedOp :: remainingUnparsedOps, Ok [] ) ->
+                    -- not started yet, multiple items to parse
+                    Result.map (addToOpList remainingUnparsedOps) (fromString nextUnparsedOp Nothing)
+
+                ( [ unparsedOp ], Ok [ parsedOp ] ) ->
+                    -- one down, one to go
+                    Result.map (\new -> new :: [ parsedOp ]) (fromString unparsedOp (Just parsedOp))
+
+                ( nextUnparsedOp :: _, Ok ((lastParsedOp :: _) as parsedOps) ) ->
+                    -- multiple done, multiple remain
+                    -- parsedOps is reversed, it's more efficient to grab a list head than the last item
+                    Result.map (\new -> new :: parsedOps) (fromString nextUnparsedOp (Just lastParsedOp))
+
+        finalList =
+            -- recursively move from one list to the other
+            -- second list is backwards for performance (ostensibly)
+            addToOpList spanOpStrings (Ok [])
+    in
+    -- TODO does having it reversed defeat the point of building it backwards?
+    Result.map List.reverse finalList
+
+
+{-| TODO determine when a chain is not a single span
+-}
+fromChain : String -> Result String (List Op)
+fromChain chain =
+    fromSpan chain
+
+
+{-| TODO determine when a chunk is not a single chain
+-}
+fromChunk : String -> Result String (List Op)
+fromChunk chunk =
+    fromChain chunk
+
+
+{-| A frame can have multiple chunks
+-}
+fromFrame : String -> Result String (List Op)
+fromFrame frame =
+    let
+        chunks =
+            String.split ";" frame
+    in
+    Result.map List.concat <| Result.Extra.combineMap fromChunk chunks
+
+
+{-| A log can have multiple frames
+-}
+fromLog : String -> Result String (List Op)
+fromLog log =
+    let
+        frames =
+            String.split "." log
+    in
+    Result.map List.concat <| Result.Extra.combineMap fromChunk frames
