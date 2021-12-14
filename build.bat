4910189e871a5c9300922fffa2d4d7392943e0a6
Jacob <jacobedwardhaig@gmail.com>
2021-12-14 11:55:02 -0500

    Switched to using powershell
    Switched to using powershell because it was more consistent with bash. An example is PS uses correct directory slashes.

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@REM cd ~\Projects\Minder\
-
-node_modules\elm\bin\elm make --output=www\elm-gui.js elm\Main.elm
-node_modules\elm\bin\elm make --output=www\elm-browserless.js elm\Browserless.elm
-node_modules\elm\bin\elm make --output=www\elm-headless.js elm\Headless.elm
-
-node_modules\.bin\elm-live.cmd elm\Main.elm --dir=www --pushstate  -- --output=www\elm-gui.js
