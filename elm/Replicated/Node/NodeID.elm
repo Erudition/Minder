1e4b313c5bebf84c10fc114ae577fe14654868db
Erudition <cnnr_dhrty@live.com>
2023-01-28 15:38:06 -0600

    Implement NodeID with real Parser

--- elm/Replicated/Node/NodeID.elm
+++ elm/Replicated/Node/NodeID.elm
@@ -1,37 +1,93 @@
-module Replicated.Node.NodeID exposing (..)
+module Replicated.Node.NodeID exposing (NodeID, bumpSessionID, fromString, generate, parser, toString)
 
+import Parser.Advanced as Parser exposing ((|.), (|=), Parser, Token(..), int, spaces, succeed, symbol, token)
 import SmartTime.Moment as Moment exposing (Moment)
 
 
 type NodeID
-    = -- never store "session" part - generate that on every run
-      -- { primus : Int, peer : Int, client : Int, session : Int }
-      NodeID String
+    = NodeID NodeIDParts
 
 
-generate : { primus : Int, peer : Int, client : Int, session : Int } -> NodeID
+{-| Not exposed - internal helper
+-}
+type alias NodeIDParts =
+    { agent : Int, device : Int, client : Int, session : Int }
+
+
+generate : { agent : Int, device : Int, client : Int, session : Int } -> NodeID
 generate record =
-    --TODO
-    NodeID "here"
+    NodeID record
 
-
-throwawayID =
-    NodeID "temp"
 
 bumpSessionID : NodeID -> NodeID
-bumpSessionID (NodeID nodeIDString) =
-    -- TODO { nodeID | session = nodeID.session + 1 }
-    NodeID (nodeIDString ++ "2")
+bumpSessionID (NodeID oldNodeID) =
+    NodeID { oldNodeID | session = oldNodeID.session + 1 }
 
 
 toString : NodeID -> String
-toString (NodeID nodeIDString) =
-    nodeIDString
+toString (NodeID nodeIDParts) =
+    let
+        sayPart prefix num =
+            if num == 0 then
+                ""
+
+            else
+                prefix ++ String.fromInt num
+    in
+    String.concat
+        [ sayPart "a" nodeIDParts.agent
+        , sayPart "d" nodeIDParts.device
+        , sayPart "c" nodeIDParts.client
+        , "s" ++ String.fromInt nodeIDParts.session
+        ]
 
 
 fromString : String -> Maybe NodeID
 fromString input =
-    if String.length input > 0 then
-        Just (NodeID input)
-    else Nothing
+    Result.toMaybe (Parser.run parser input)
 
+
+type alias NodeIDParser a =
+    Parser Context ParseProblem a
+
+
+type Context
+    = Definition String
+    | List
+    | Record
+
+
+type ParseProblem
+    = BadCounter
+    | BadNodeID
+    | ExpectingCounter
+
+
+parser : NodeIDParser NodeID
+parser =
+    Parser.map NodeID <|
+        succeed NodeIDParts
+            |= Parser.oneOf
+                [ succeed identity
+                    |. symbol (Token "a" BadNodeID)
+                    |= int ExpectingCounter BadCounter
+                , succeed 0
+                ]
+            |= Parser.oneOf
+                [ succeed identity
+                    |. symbol (Token "d" BadNodeID)
+                    |= int ExpectingCounter BadCounter
+                , succeed 0
+                ]
+            |= Parser.oneOf
+                [ succeed identity
+                    |. symbol (Token "c" BadNodeID)
+                    |= int ExpectingCounter BadCounter
+                , succeed 0
+                ]
+            |= Parser.oneOf
+                [ succeed identity
+                    |. symbol (Token "s" BadNodeID)
+                    |= int ExpectingCounter BadCounter
+                , succeed 0
+                ]
