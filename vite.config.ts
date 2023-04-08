130349ecaa057db4ea518be9ad09640d2e218579
Erudition <cnnr_dhrty@live.com>
2023-04-08 16:31:47 -0500

    Stop service worker for now

--- vite.config.ts
+++ vite.config.ts
@@ -8,28 +8,31 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   // identify what plugins we want to use
   plugins: [
-    VitePWA({ registerType: 'autoUpdate',
-        //devOptions: {enabled: true},
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-        manifest: {
-          name: 'Minder Prototype',
-          short_name: 'Minder',
-          description: 'Mental Assistant',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: 'android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        }
-    }),
+    // VitePWA({ registerType: 'autoUpdate',
+    //     //devOptions: {enabled: true},
+    //     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    //     workbox: {
+    //       globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    //     },
+    //     manifest: {
+    //       name: 'Minder Prototype',
+    //       short_name: 'Minder',
+    //       description: 'Mental Assistant',
+    //       theme_color: '#ffffff',
+    //       icons: [
+    //         {
+    //           src: 'android-chrome-192x192.png',
+    //           sizes: '192x192',
+    //           type: 'image/png'
+    //         },
+    //         {
+    //           src: 'android-chrome-512x512.png',
+    //           sizes: '512x512',
+    //           type: 'image/png'
+    //         }
+    //       ]
+    //     }
+    // }),
     elmPlugin({debug: true}),
     nodePolyfills({
         // Whether to polyfill `node:` protocol imports.
@@ -64,4 +67,7 @@ export default defineConfig({
     ],
     force: true
   },
+  server: {
+    strictPort: true
+  }
 })
