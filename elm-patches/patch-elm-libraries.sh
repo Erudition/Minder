b787a83149ea4beda5f0ef43de4bf378feaa2133
Erudition <cnnr_dhrty@live.com>
2023-11-15 21:24:30 -0600

    Fix patching in CI where .elm-land doesn't exist

--- elm-patches/patch-elm-libraries.sh
+++ elm-patches/patch-elm-libraries.sh
@@ -1,7 +1,9 @@
 # TODO only run if pathces are not already applied
 
-# TODO how to get elm to re-compute only the selected packages without blowing them all away
-rm -rf ~/.elm/0.19.1/packages/ && \
-elm make --output=/dev/null elm-patches/dummyproject/Dummy.elm && \
-make --directory ./elm-patches && \
-rm -rf elm-stuff
+
+rm -rf ~/.elm/0.19.1/packages/ # TODO how to get elm to re-compute only the selected packages without blowing them all away
+cd elm-patches/dummyproject/ # CI env is empty so we use minimal elm.json for dummy project
+elm make --output=/dev/null Dummy.elm # build dummy project and output to nowhere, so libraries are re-downloaded
+cd ../.. # go back to project dir
+make --directory ./elm-patches # apply patches
+rm -rf elm-stuff # get rid of cached elm builds so project will create fresh ones with patches
