73af75624d409cc257af3152c743eb8f1b084011
Erudition <cnnr_dhrty@live.com>
2023-11-20 16:21:37 -0600

    Make elm patching more robust

--- elm-patches/patch-elm-libraries.sh
+++ elm-patches/patch-elm-libraries.sh
@@ -3,6 +3,7 @@
 
 rm -rf ~/.elm/0.19.1/packages/ # TODO how to get elm to re-compute only the selected packages without blowing them all away
 cd elm-patches/dummyproject/ # CI env is empty so we use minimal elm.json for dummy project
+rm -rf elm-stuff # get rid of cache in dummy project
 elm make --output=/dev/null Dummy.elm # build dummy project and output to nowhere, so libraries are re-downloaded
 cd ../.. # go back to project dir
 make --directory ./elm-patches # apply patches
